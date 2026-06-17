7233f77e5a718eecc3218e8f8dc3287cdefce0d5
Nikk0o <nknk@lagartao.gay>
2026-06-17 00:12:40 -0300

    Added FAQ.jsx

---
+++ frontend/src/components/FAQ.jsx
@@ -0,0 +1,107 @@
+function FAQ() {
+	return (
+	  <section className="my-0 mx-auto max-w-[950px] font-viminalis py-0 px-20">
+	  <details className="border-b-1 border-lfapink">
+		<summary className="flex justify-between items-center py-16px px-0 text-[1.2rem] text-lfapink curso-pointer list-image-none">
+		Como comprar?
+		<svg className="fill-lfapink transition-transform duration-[0.3s] ease-in-out" viewBox="0 0 24 24" width="24" height="24">
+		  <path fill="lfapink" d="M9 18l6-6-6-6"/>
+		</svg>
+		</summary>
+		<div className="pb-16 text-[1.6]">
+		<p>Após escolher os produtos você pode fazer seu pela loja ou então nos enviar um WhatsApp.</p>
+		</div>
+	  </details>
+	  <details className="border-b-1 border-lfapink">
+		<summary className="faq-pergunta">
+		Como é feita a entrega?
+		<svg className="faq-icone" viewBox="0 0 24 24" width="24" height="24">
+		  <path fill="currentColor" d="M9 18l6-6-6-6"/>
+		</svg>
+		</summary>
+		<div className="pb-16 text-1.6">
+		<p>A entrega é feita pelos correios ou pela transportadora Jadlog. Para moradores de Ribeirão Preto, o produto pode ser retirado no local.</p>
+		</div>
+	  </details>
+	  <details className="border-b-1 border-lfapink">
+		<summary className="faq-pergunta">
+		Como pagar?
+		<svg className="faq-icone" viewBox="0 0 24 24" width="24" height="24">
+		  <path fill="currentColor" d="M9 18l6-6-6-6"/>
+		</svg>
+		</summary>
+		<div className="pb-16 text-1.6">
+		<p>O pagamento (100% do valor da encomenda + frete) pode ser feito por pix, boleto ou cartão de crédito. Por cartão é possível fazer parcelamento, mas fique atento porque há cobrança de juros.</p>
+		</div>
+	  </details>
+	  <details className="border-b-1 border-lfapink">
+		<summary className="faq-pergunta">
+		Qual o prazo de produção?
+		<svg className="faq-icone" viewBox="0 0 24 24" width="24" height="24">
+		  <path fill="currentColor" d="M9 18l6-6-6-6"/>
+		</svg>
+		</summary>
+		<div className="pb-16 text-1.6">
+		<p>O prazo de produção dos produtos é de 7 a 10 dias úteis, e entrará em vigor somente após a confirmação do pagamento e aprovação de todos os modelos. Após esse tempo, a encomenda será postada nos correios.</p>
+		</div>
+	  </details>
+	  <details className="border-b-1 border-lfapink">
+		<summary className="faq-pergunta">
+		O que é considerado confirmação do pagamento?
+		<svg className="faq-icone" viewBox="0 0 24 24" width="24" height="24">
+		  <path fill="currentColor" d="M9 18l6-6-6-6"/>
+		</svg>
+		</summary>
+		<div className="pb-16 text-1.6">
+		<p>PIX: assim que o dinheiro for liberado na conta.<br></br>Cartão de Crédito: assim que o Mercado Pago enviar a confirmação por e-mail.<br></br>Boleto Bancário: pagamento leva até 3 dias úteis para a confirmação.</p>
+		</div>
+	  </details>
+	  <details className="border-b-1 border-lfapink">
+		<summary className="faq-pergunta">
+		Vou receber um modelo antes da minha compra ser enviada?
+		<svg className="faq-icone" viewBox="0 0 24 24" width="24" height="24">
+		  <path fill="currentColor" d="M9 18l6-6-6-6"/>
+		</svg>
+		</summary>
+		<div className="pb-16 text-1.6">
+		<p>Sim, após a confirmação do pagamento você irá receber um modelo por WhatsApp de todos os itens comprados, para você conferir e aprovar.</p>
+		</div>
+	  </details>
+	  <details className="border-b-1 border-lfapink">
+		<summary className="faq-pergunta">
+		Depois que eu aprovar os modelos, posso fazer alguma alteração?
+		<svg className="faq-icone" viewBox="0 0 24 24" width="24" height="24">
+		  <path fill="currentColor" d="M9 18l6-6-6-6"/>
+		</svg>
+		</summary>
+		<div className="pb-16 text-1.6">
+		<p>Nós fazemos todas as alterações necessárias até que tudo fique do seu gosto, mas assim que você aprovar o modelo, iniciamos a produção e não será mais possível alterar nada. Por isso, é muito importante conferir atentamente cada item.</p>
+		</div>
+	  </details>
+	  <details className="border-b-1 border-lfapink">
+		<summary className="faq-pergunta">
+		E se eu fizer o pedido em cima da hora e tiver algum atraso na entrega?
+		<svg className="faq-icone" viewBox="0 0 24 24" width="24" height="24">
+		  <path fill="currentColor" d="M9 18l6-6-6-6"/>
+		</svg>
+		</summary>
+		<div className="pb-16 text-1.6">
+		<p>Não nos responsabilizamos por eventuais atrasos por parte dos correios ou da JadLog, por isso, fique atento ao prazo de produção e ao prazo de entrega para planejar o seu pedido com a antecedência necessária.</p>
+		</div>
+	  </details>
+	  <details className="border-b-1 border-lfapink">
+		<summary className="faq-pergunta">
+		Gostei muito do bloquinho de safári, mas a festa do meu filho será de pirata. É possível fazer com esse tema?
+		<svg className="faq-icone" viewBox="0 0 24 24" width="24" height="24">
+		  <path fill="currentColor" d="M9 18l6-6-6-6"/>
+		</svg>
+		</summary>
+		<div className="pb-16 text-1.6">
+		<p>Sim, todos os itens podem ser feitos em qualquer tema que tenha na loja.</p>
+		</div>
+	  </details>
+      </section>
+	)
+}
+
+export default FAQ
--- frontend/src/layouts/RootLayout.jsx
+++ frontend/src/layouts/RootLayout.jsx
@@ -2,7 +2,8 @@ import { Outlet, useNavigation } from 'react-router'
 
 import Header from "../components/Header.jsx";
 import Footer from "../components/Footer.jsx";
-import Loading from "../components/Loading.jsx"
+import Loading from "../components/Loading.jsx";
+import FAQ from "../components/FAQ.jsx"
 
 export default function RootLayout() {
   const navigation = useNavigation();
@@ -16,6 +17,7 @@ export default function RootLayout() {
       <>
       <Header />
       <Outlet/>
+	  <FAQ />
       <Footer />
       </>
     )}
